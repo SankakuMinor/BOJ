ff110a7ac77c44ab452c87ef921bb44955451e04
Sangmin Im <owenkicc@gmail.com>
2025-11-29 01:28:15 +0900

    Create 10807.cpp

---
+++ 10807.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[101]={};
+    int v;
+    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }   
+    cin>>v;
+    
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==v) ans++;
+    }
+    cout<<ans;
+    return 0;
+}
