3d8409de96bdadf3811588573cf1ace0a5a27b79
Sangmin Im <owenkicc@gmail.com>
2025-11-29 01:47:19 +0900

    Create 2884.cpp

---
+++ 2884.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    if(b>=45){
+        cout<<a<<" "<<b-45;
+    }
+    else{
+        if(a==0)
+            cout<<"23 "<<b+15;
+        else
+            cout<<a-1<<" "<<b+15;
+    }
+}
