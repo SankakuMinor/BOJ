10fae5fb64a1507483212e8025ea2c9f9bb8ea8e
Sangmin Im <owenkicc@gmail.com>
2025-11-29 01:45:45 +0900

    Create 15649.cpp

---
+++ 15649.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+bool visited[9];
+int a[9];
+
+int m, n, cnt;
+
+void dfs()
+{
+    if(cnt==n){
+        for(int i=1;i<=n;i++){
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+        return;
+    }
+    else{
+        for(int i=1;i<=m;i++){
+            if(visited[i]==0){
+                visited[i]=1;
+                a[++cnt]=i;
+                dfs();
+                a[cnt]=0;
+                cnt--;
+                visited[i]=0;
+            }
+        }
+    }
+}
+int main()
+{
+    scanf("%d %d", &m, &n);
+    dfs();
+}
